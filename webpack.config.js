37746ba984371f9d7c7b00853e9e6d5bcc709718
Zach Orlovsky <sadorlovsky@gmail.com>
2016-05-30 01:38:45 +0300

    use eval-source-map in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'eval-source-map',
   entry: [
     'react-hot-loader/patch',
     'webpack-hot-middleware/client?reload=true',
