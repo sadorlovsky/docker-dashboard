f4800b1b106a11933371b419a032e432fa8e5a1a
Zach Orlovsky <sadorlovsky@gmail.com>
2016-05-31 09:19:46 +0300

    ui wip

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import ContainerList from './ContainerList'
 import ContainerDetail from './ContainerDetail'
+import Layout from './Layout'
 
 const App = () => {
   return (
     <Router history={browserHistory}>
-      <Route path='/' component={props => <div>{props.children}</div>}>
+      <Route path='/' component={Layout}>
         <IndexRoute component={ContainerList} />
         <Route path='containers' component={ContainerList} />
         <Route path='container/:containerId' component={ContainerDetail} />
