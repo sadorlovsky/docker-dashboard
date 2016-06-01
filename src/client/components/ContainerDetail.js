ddba1fe05b211631ffe124da39d78b1f2db789a7
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-01 14:00:45 +0300

    container detail view

--- src/client/components/ContainerDetail.js
+++ src/client/components/ContainerDetail.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import { connect } from 'react-apollo'
+import css from 'react-css-modules'
 import gql from 'apollo-client/gql'
 import Spinner from 'react-spinkit'
-import Container from './Container'
+import styles from './ContainerDetail.sss'
 
 const ContainerDetail = ({ data }) => {
   if (data.loading) {
     return <Spinner spinnerName='wandering-cubes' noFadeIn />
   }
-  return <Container container={data.getContainer} />
+  return (
+    <div>
+      <div styleName='container'>
+        <div>ID: {data.container.id}</div>
+        <div>NAME: {data.container.name}</div>
+        <div>IMAGE: {data.container.image}</div>
+        <div>RUNNING: {data.container.running.toString()}</div>
+        <div>COMMAND: {data.container.command}</div>
+      </div>
+      <div>
+        <button>{data.container.running ? 'stop' : 'start'}</button>
+      </div>
+    </div>
+  )
 }
 
 const mapQueriesToProps = ({ ownProps }) => {
@@ -16,7 +30,7 @@ const mapQueriesToProps = ({ ownProps }) => {
     data: {
       query: gql`
         query Query($containerId: String!) {
-          getContainer(id: $containerId) {
+          container: getContainer(id: $containerId) {
             id
             name
             image
@@ -36,4 +50,4 @@ const mapQueriesToProps = ({ ownProps }) => {
 
 export default connect({
   mapQueriesToProps
-})(ContainerDetail)
+})(css(ContainerDetail, styles))
