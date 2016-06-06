4079edfb79c1b81af513c5d5dcafeec0b81ae0f7
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-06 06:22:53 +0300

    add document title

--- src/client/components/DockerContainer/Detail/Detail.js
+++ src/client/components/DockerContainer/Detail/Detail.js
@@ -4,6 +4,7 @@ import css from 'react-css-modules'
 import gql from 'apollo-client/gql'
 import Spinner from 'react-spinkit'
 import compose from 'recompose/compose'
+import DocumentTitle from 'react-document-title'
 import { toggleId } from '../../../actions'
 import ContainerId from './ContainerId'
 import ToggleIdButton from './ToggleIdButton'
@@ -14,21 +15,23 @@ const ContainerDetail = ({ data, showFullId, handleClick }) => {
     return <Spinner spinnerName='wandering-cubes' />
   }
   return (
-    <div>
-      <div styleName='container'>
-        <div styleName='name'>{data.container.name}</div>
-        <div>
-          <ContainerId id={data.container.id} showFullId={showFullId} />
-          <ToggleIdButton showFullId={showFullId} handleClick={handleClick} />
-        </div>
-        <div>IMAGE: {data.container.image}</div>
-        <div>RUNNING: {data.container.running.toString()}</div>
-        <div>COMMAND: {data.container.command}</div>
-      </div>
+    <DocumentTitle title={`${data.container.name} · Docker Dashboard`}>
       <div>
-        <button>{data.container.running ? 'stop' : 'start'}</button>
+        <div styleName='container'>
+          <div styleName='name'>{data.container.name}</div>
+          <div>
+            <ContainerId id={data.container.id} showFullId={showFullId} />
+            <ToggleIdButton showFullId={showFullId} handleClick={handleClick} />
+          </div>
+          <div>IMAGE: {data.container.image}</div>
+          <div>RUNNING: {data.container.running.toString()}</div>
+          <div>COMMAND: {data.container.command}</div>
+        </div>
+        <div>
+          <button>{data.container.running ? 'stop' : 'start'}</button>
+        </div>
       </div>
-    </div>
+    </DocumentTitle>
   )
 }
 
--- src/client/components/Layout/Layout.js
+++ src/client/components/Layout/Layout.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import DocumentTitle from 'react-document-title'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 
 const Layout = props => {
   return (
-    <div>
-      <Header />
-      {props.children}
-      <Footer />
-    </div>
+    <DocumentTitle title='Docker Dashboard'>
+      <div>
+        <Header />
+        {props.children}
+        <Footer />
+      </div>
+    </DocumentTitle>
   )
 }
 
