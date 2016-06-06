5cb14f6abf9d4b390658c0c484fe54dac07ab1f3
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-06 07:12:42 +0300

    use in-memory datastore for mocking

--- src/server/mocks.js
+++ src/server/mocks.js
@@ -1,17 +1,41 @@
 import faker from 'faker'
-import { MockList } from 'graphql-tools'
+import NeDB from 'nedb'
+import times from 'lodash/times'
+
+const db = new NeDB()
+const count = faker.random.number({ min: 10, max: 50 })
+const docs = times(count, () => ({
+  id: faker.random.uuid(),
+  name: faker.helpers.slugify(faker.random.word()),
+  image: faker.helpers.slugify(faker.random.word()),
+  imageId: faker.random.uuid(),
+  running: faker.helpers.randomize([true, false]),
+  command: faker.hacker.phrase()
+}))
+db.insert(docs, err => {
+  if (err) {
+    console.log(err)
+  }
+})
 
 const mocks = {
   Query: () => ({
-    getContainerList: () => new MockList([10, 50])
-  }),
-  Container: () => ({
-    id: () => faker.random.uuid(),
-    name: () => faker.helpers.slugify(faker.random.word()),
-    image: () => faker.helpers.slugify(faker.random.word()),
-    imageId: () => faker.random.uuid(),
-    running: () => faker.helpers.randomize([true, false]),
-    command: () => faker.hacker.phrase()
+    getContainerList: () => new Promise((resolve, reject) => {
+      return db.find({}, (err, containers) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(containers)
+      })
+    }),
+    getContainer: (_, { id }) => new Promise((resolve, reject) => {
+      return db.findOne({ id: id }, (err, _container) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(_container)
+      })
+    })
   })
 }
 
