0745960cc580526da1760ecd02344f078179a772
Zach Orlovsky <sadorlovsky@gmail.com>
2016-11-16 06:45:50 +0300

    remove prefetch on filter

--- src/client/components/ContainerList/Filter.js
+++ src/client/components/ContainerList/Filter.js
@@ -1,39 +1,24 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import { compose } from 'redux'
-import { withApollo } from 'react-apollo'
-import getContainers from '../../queries/getContainers'
 import { toggleFilter } from '../../actions'
 
-const Filter = ({ filter, onChangeFilter, client }) => {
-  const hoverHandle = type => {
-    client.query({
-      query: getContainers,
-      variables: {
-        filterType: type
-      }
-    })
-  }
-
+const Filter = ({ filter, onChangeFilter }) => {
   return (
     <Button.Group fluid>
       <Button
-        onMouseOver={() => hoverHandle('running')}
         onClick={() => onChangeFilter('running')}
         active={filter === 'running'}
       >
           Running
         </Button>
       <Button
-        onMouseOver={() => hoverHandle('stopped')}
         onClick={() => onChangeFilter('stopped')}
         active={filter === 'stopped'}
       >
           Stopped
         </Button>
       <Button
-        onMouseOver={() => hoverHandle('all')}
         onClick={() => onChangeFilter('all')}
         active={filter === 'all'}
       >
@@ -43,16 +28,13 @@ const Filter = ({ filter, onChangeFilter, client }) => {
   )
 }
 
-const enhancer = compose(
-  withApollo,
-  connect(
-    ({ rootReducer }) => ({ filter: rootReducer.filter }),
-    dispatch => ({
-      onChangeFilter (type) {
-        dispatch(toggleFilter(type))
-      }
-    })
-  )
+const enhancer = connect(
+  ({ rootReducer }) => ({ filter: rootReducer.filter }),
+  dispatch => ({
+    onChangeFilter (type) {
+      dispatch(toggleFilter(type))
+    }
+  })
 )
 
 export default enhancer(Filter)
