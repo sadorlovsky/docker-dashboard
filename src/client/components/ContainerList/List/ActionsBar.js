2348673c28b752fe41ec58c31517b2761945c4bd
Zach Orlovsky <sadorlovsky@gmail.com>
2016-12-24 23:03:33 +0300

    add actions to actions bar

--- src/client/components/ContainerList/List/ActionsBar.js
+++ src/client/components/ContainerList/List/ActionsBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 import { style } from 'glamor'
 import { connect } from 'react-redux'
 import plural from 'plural'
@@ -11,13 +12,23 @@ const styles = style({
   bottom: '0',
   height: '50px',
   backdropFilter: 'blur(2px)',
-  fontSize: '18px'
+  fontSize: '18px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between'
 })
 
 const ActionsBar = ({ selectedContainers }) => {
   if (selectedContainers.length > 0) {
     return (
-      <div {...styles}>Selected {selectedContainers.length} {plural('container', selectedContainers.length)}</div>
+      <div {...styles}>
+        <div>Selected {selectedContainers.length} {plural('container', selectedContainers.length)}</div>
+        <Button.Group>
+          <Button basic inverted>Stop</Button>
+          <Button basic inverted color='yellow'>Restart</Button>
+          <Button basic inverted color='red'>Delete</Button>
+        </Button.Group>
+      </div>
     )
   }
   return null
